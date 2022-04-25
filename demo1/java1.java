8605f5b9cd59ec14756aac1d11059c1eddcc3b99
Khushiii23 <khushipatel23aadesh@gmail.com>
2022-04-25 14:39:52 +0530

    folder added

---
+++ demo1/java1.java
@@ -0,0 +1,21 @@
+package demo1;
+
+import java.util.Scanner;
+
+public class java1 {
+	public static void main(String[]args)
+	{
+int ;
+	Scanner sc = new Scanner( System.in) ;
+	System.out.println("enter value of a");
+	a= sc.nextInt();
+	System.out.println("enter value of b");
+	b= sc.nextInt();
+	if(a<21)
+		System.out.println("yoy are eligble for voting");
+	else
+		System.out.println("yoy are not eligble for voting");
+	
+	}
+
+}
---
+++ demo1/java3.java
@@ -0,0 +1,71 @@
+package demo1;
+import java.util.scanner;
+public class java3 {
+	public static void main(String []args) {
+
+		System.out.println("1.pizza");
+		System.out.println("2.pasta");
+		System.out.println("3.burger");
+		System.out.println("4.dosa");
+		System.out.println("5.daipuri");
+Scanner.sc= new Scanner(System.in);
+System.out.println("your choice");
+int dish= Sc.nextInt();
+if(dish==1)
+{
+	System.out.println("pizza");
+System.out.println("enter quantity");
+int quan=sc.nextInt();
+int c= quan*200;
+System.out.println(" Total quantity is= "+c);
+}
+if(dish==2)
+{
+	System.out.println("pasta");
+	System.out.println("enter quantity");
+	int quan=sc.nextInt();
+	int c= quan*90;
+	System.out.println(" Total quantity is= "+c);
+}
+if(dish==3)
+{
+	System.out.println("burger");
+	System.out.println("enter quantity");
+	int quan=sc.nextInt();
+	int c= quan*46;
+	System.out.println(" Total quantity is= "+c);
+}
+if(dish==4)
+{
+	System.out.println("dosa");
+	System.out.println("enter quantity");
+	int quan=sc.nextInt();
+	int c= quan*200;
+	System.out.println(" Total quantity is= "+c);
+}
+if(dish==5)
+{
+	System.out.println("daipuri");
+	System.out.println("enter quantity");
+	int quan=sc.nextInt();
+	int c= quan*200;
+	System.out.println(" Total quantity is= "+c);
+}
+if(dish==6)
+{
+System.out.println("all");
+System.out.println(" enter quantity of pizza");
+int quan= sc.nextInt();
+
+
+
+
+	
+
+
+
+
+		
+		
+
+}
